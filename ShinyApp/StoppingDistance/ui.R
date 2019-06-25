4418ed3298db7b7ffc8a7dff41e5c342e2623065
Alán García Bernal <alangarciabernal@gmail.com>
2019-06-24 23:02:37 -0500

    Added Second project
    Second App using 'shinydashboard'. This commit includes only the ui.R finished.

--- ShinyApp/StoppingDistance/ui.R
+++ ShinyApp/StoppingDistance/ui.R
@@ -1,23 +1,33 @@
 library(shiny)
+library(shinydashboard)
+library(plotly)
 
-
-shinyUI(fluidPage(
-    titlePanel("Predict Stopping Distance from Car Speed"),
-    sidebarLayout(
-        sidebarPanel(
-            fluidRow(
-                column(width = 8,
-                       sliderInput("sliderSpeed", "What is the speed of the car?", 1,25, value = 10),
-                       box(selectInput("model", "Choose Model:", c("Linear Regression" = "modelA",
-                                                                   "Spline Regression" = "modelB"), width=NULL)) 
-                       )
-            )
-        ),
-        mainPanel(
-            plotOutput("plotA"),
-            h3("Predicted Stopping Distance for selected Distance"),
-            textOutput("pred")
+shinyUI(dashboardPage(
+    dashboardHeader(title = "Stopping Distance for Car Speeds",
+                    titleWidth = 450),
+    dashboardSidebar(disable = TRUE),
+    dashboardBody(
+        tags$head(tags$style(HTML('
+    /* logo */
+        .skin-blue .main-header .logo {
+                              background-color: #006b6f;
+                              font-family: Courier;
+                              }
+    /* navbar (rest of the header) */
+        .skin-blue .main-header .navbar {
+                              background-color: #00868B;
+                              }'))),
+        fluidRow(
+            column(width = 8,
+                   box(plotlyOutput("forecastPlot"), width = NULL),
+                   box(plotlyOutput("resPlot"), width = NULL)),
+            column(width = 4,
+                   box(selectInput("model","Select Regression Model:",
+                                   c("Linear Regression" = "fit.lm",
+                                     "Spline Regression" = "fit.sp")),
+                       width = NULL),
+                   box(verbatimTextOutput("test.output"),
+                       width = NULL))
         )
     )
-    
 ))
